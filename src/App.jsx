96559bdee05cfdb50a3cf41df51be65fd2d480af
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-11-27 11:02:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:6100/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Simple CRUD</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input name="name" type="text" />
+        <br />
+        <input name="email" type="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
+  );
+}
+
+export default App;
